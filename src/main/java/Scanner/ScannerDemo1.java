6fe85843bbfd7acd4d92303553acc91a644ac360
franzlee <164681434@qq.com>
2022-11-02 18:57:06 +0800

    Initial commit

---
+++ src/main/java/Scanner/ScannerDemo1.java
@@ -0,0 +1,29 @@
+package Scanner;
+
+/*
+  @author lin
+ * @date 2022/11/02/16:00
+ */
+
+//1.导包，找到Scanner这个类在哪儿
+//书写要注意：要写在类定义的上面
+import java.util.Scanner;
+
+/**
+ * @author franzlee
+ */
+public class ScannerDemo1 {
+    public static void main(String[] args) {
+        //2.创建对象，标识现在准备要用Scanner这个类
+        Scanner sc = new Scanner(System.in);
+
+        /*
+        3.接收数据
+        变量i记录了键盘录入的数据
+        */
+        int i = sc.nextInt();
+
+        //4.打印i输入的内容信息
+        System.out.println(i);
+    }
+}
---
+++ src/main/java/Scanner/ScannerTest.java
@@ -0,0 +1,28 @@
+package Scanner;
+
+//导入Scanner包
+import java.util.Scanner;
+
+/**
+ * @author franzlee
+ */
+public class ScannerTest {
+    /**
+     * 主入口
+     */
+    public static void main(String[] args) {
+        /*创建对象*/
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("请输入第一个数字：");
+        //接收number1数据
+        int number1 = sc.nextInt();
+
+        System.out.print("请输入第二个数字：");
+        //接受number2数据
+        int number2 = sc.nextInt();
+
+        //输出number1 + number2 总和的数
+        System.out.print(number1 + number2);
+    }
+}
