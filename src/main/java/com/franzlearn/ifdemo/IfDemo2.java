938b9cef6aaea6d5bc1f7fb6ac4e911b50831218
franzlee <164681434@qq.com>
2022-11-11 20:58:04 +0800

    Initial commit

--- src/main/java/com/franzlearn/ifdemo/IfDemo2.java
+++ src/main/java/com/franzlearn/ifdemo/IfDemo2.java
@@ -15,14 +15,19 @@ package com.franzlearn.ifdemo;
     if的重点：
         1.大括号的开头可以另起一行书写，但是建议写在第一行的末尾
         2.在语句中，如果只有一句代码，大括号可以省略不写
-        3.如果对一个布尔类型的变量进行判断，不要用==号
+        3.如果对一个布尔类型的变量进行判断，不要用==号。直接把变量写在小括号即可
  */
 
 public class IfDemo2 {
     public static void main(String[] args) {
-        int number = 10;
-        if (number >= 10) {
-            System.out.println("number大于等于10");
+//        int number = 10;
+//        if (number >= 10) {
+//            System.out.println("number大于等于10");
+//        }
+
+        boolean flag = false;
+        if (flag == true) {
+            System.out.println("flag的值为true");
         }
     }
 
